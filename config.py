b2cdb8b17ead895d08e84a355238583dd84c05c8
RAT <158288653+Surya26668@users.noreply.github.com>
2024-08-30 23:59:39 +0700

    Update config.py
    Signed-off-by: RAT <158288653+Surya26668@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = 20409880
 api_hash = 'b30c20d8ff623d3cf30d7cc011e1684b'
-owner = '@SuryaK3'
+owner = '@'
 bot_username = '@'
 mongo_uri = 'mongodb+srv://mr_kuyuk:galangsurya1309@cluster0.uhvesrz.mongodb.net/converter?retryWrites=true&w=majority&appName=Cluster0'
